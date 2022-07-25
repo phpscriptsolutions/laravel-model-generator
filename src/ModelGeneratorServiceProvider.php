1469d2e6c3363ffc880422ae4c91fbbb0f69e292
Nguyen Trung Hieu <hieu.nt2272@gianty.com.vn>
2022-07-25 12:41:25 +0700

    update

--- src/ModelGeneratorServiceProvider.php
+++ src/ModelGeneratorServiceProvider.php
@@ -1,28 +1,27 @@
 <?php
 
-namespace PhpScriptSolutions\ModelGenerator {
+namespace PhpScriptSolutions\ModelGenerator
 
-    use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-    class ModelGeneratorServiceProvider extends ServiceProvider
+class ModelGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
     {
-        /**
-         * Bootstrap the application services.
-         */
-        public function boot()
-        {
-        }
+    }
 
-        /**
-         * Register the application services.
-         */
-        public function register()
-        {
-            $this->app->singleton('command.make.models', function ($app) {
-                return $app['PhpScriptSolutions\Generator\Commands\MakeModelsCommand'];
-            });
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->singleton('command.make.models', function ($app) {
+            return $app['PhpScriptSolutions\Generator\Commands\MakeModelsCommand'];
+        });
 
-            $this->commands('command.make.models');
-        }
+        $this->commands('command.make.models');
     }
 }
